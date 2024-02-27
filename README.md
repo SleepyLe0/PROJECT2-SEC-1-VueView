7a2ebdbf79d1cef147a1207ac55b492c32596c7f
SleepyLe0 <kunkundids@gmail.com>
2024-02-27 16:34:33 +0700

    project starter

---
+++ README.md
@@ -0,0 +1,6 @@
+# Group Members
+1. 65130500005 Kundids Khawmeesri 
+2. 65130500006 Kantaphong Polmanee
+3. 65130500039 Niraphan Duangchairakkasem
+4. 65130500040 Benyaporn Sangkhiao
+5. 65130500045 Pongsapat Charoencharuskul
---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref } from 'vue'
+
+const colorTheme = ref(true)
+</script>
+
+<template>
+  <div class="flex flex-col justify-center items-center h-screen gap-10" :data-theme=" colorTheme ? 'light' : 'dark'">
+    <h1 class="font-bold text-3xl">Tailwind CSS Test</h1>
+    <input type="checkbox" class="toggle" v-model="colorTheme">
+  </div>
+</template>
+
+<style scoped>
+
+</style>
