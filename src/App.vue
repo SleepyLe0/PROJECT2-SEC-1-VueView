686af921803f265192821c1e20f03fe4f0801942
SleepyLe0 <kunkundids@gmail.com>
2024-03-10 21:57:03 +0700

    starter project

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { ref } from 'vue'
+import Basic from './components/Basic.vue'
 
 const colorTheme = ref(true)
 </script>
@@ -8,6 +9,9 @@ const colorTheme = ref(true)
   <div class="flex flex-col justify-center items-center h-screen gap-10" :data-theme=" colorTheme ? 'light' : 'dark'">
     <h1 class="font-bold text-3xl">Tailwind CSS Test</h1>
     <input type="checkbox" class="toggle" v-model="colorTheme">
+    <Basic>
+      <template #greeting>Hello</template>
+    </Basic>
   </div>
 </template>
 
---
+++ src/components/Basic.vue
@@ -0,0 +1,22 @@
+<script setup>
+const props = defineProps({
+    username: {
+        type: String,
+        default: 'guest'
+    }
+})
+</script>
+
+<template>
+    <div class="flex flex-col justify-center items-center gap-5">
+        <h1 class="text-5xl">This is Basic Component.</h1>
+        <div class="text-4xl flex gap-3">
+            <slot name="greeting"></slot>
+            <h2>{{ props.username }}</h2>
+        </div>        
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
