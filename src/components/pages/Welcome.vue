f7df8cdecb4ced4314fa4f83c595beb5dd0cddc4
SleepyLe0 <kunkundids@gmail.com>
2024-03-13 21:38:48 +0700

    add welcome page

---
+++ src/components/pages/Welcome.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="flex justify-center items-center text-5xl font-bold">
+        <h1>This is welcome</h1>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
--- src/router.js
+++ src/router.js
@@ -1,13 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Login from './components/pages/Login.vue'
+import Welcome from './components/pages/Welcome.vue'
 import Home from './components/pages/Home.vue'
+import Login from './components/pages/Login.vue'
 import Inventory from './components/pages/Inventory.vue'
 import Level from './components/pages/Level.vue'
 import Setting from './components/pages/Setting.vue'
 import Shop from './components/pages/Shop.vue'
 
 const routes = [
-    { path: '/', component: Home },
+    { path: '/', component: Welcome },
+    { path: '/home', component: Home },
     { path: '/login', component: Login },
     { path: '/inventory', component: Inventory },
     { path: '/level', component: Level },
