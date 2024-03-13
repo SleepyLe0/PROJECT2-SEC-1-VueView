a2a4f999b08f9b7088fead945ae7a82f637bb4a6
SleepyLe0 <kunkundids@gmail.com>
2024-03-13 20:14:00 +0700

    modified data

--- README.md
+++ README.md
@@ -1,16 +1,15 @@
 # Group Members
 1. 65130500005 Kundids Khawmeesri - Turn-Based
 2. 65130500006 Kantaphong Polmanee - Shop
-3. 65130500039 Niraphan Duangchairakkasem - Account System | Setting
+3. 65130500039 Niraphan Duangchairakkasem - Account System
 4. 65130500040 Benyaporn Sangkhiao - Inventory | Home
-5. 65130500045 Pongsapat Charoencharuskul - Achievement
+5. 65130500045 Pongsapat Charoencharuskul - Setting
 
 # Main Features
 1. Turn-Based (5 Stage)
 2. Account System
 3. Shop
 4. Inventory
-5. Achievement
 
 # Theme
 - Main Theme : Fantasy
---
+++ src/assets/monsters.js
@@ -0,0 +1,44 @@
+const monsters = [
+    {
+        id: "1",
+        name: "Lizard Man",
+        hp: 100,
+        attack: 10,
+        level: 1,
+        image: "/Monster/LizardMan.png"
+    },
+    {
+        id: "2",
+        name: "Jhin",
+        hp: 80,
+        attack: 20,
+        level: 2,
+        image: "/Monster/Jhin.png"
+    },
+    {
+        id: "3",
+        name: "Medusa",
+        hp: 60,
+        attack: 30,
+        level: 3,
+        image: "/Monster/Medusa.png"
+    },
+    {
+        id: "4",
+        name: "Dragon",
+        hp: 150,
+        attack: 15,
+        level: 4,
+        image: "/Monster/Dragon.png"
+    },
+    {
+        id: "5",
+        name: "Demon",
+        hp: 200,
+        attack: 10,
+        level: 5,
+        image: "/Monster/Demon.png"
+    }
+]
+
+export default monsters
