454863b828a411959be6a7bdfd7c9ec448aa2632
Umbyp <110766551+Umbyp@users.noreply.github.com>
2024-03-14 01:49:48 +0700

    edit cardcharacter adn edit inventory

--- src/components/common/Cardcharater.vue
+++ src/components/common/Cardcharater.vue
@@ -3,13 +3,13 @@ import heros from '../../assets/heros'
 </script>
 
 <template>
-  <div class="flex text-black justify-center items-center gap-3 ">
+  <div class="flex text-black justify-center items-center gap-2 ">
     <div v-for="hero in heros" :key="hero.id"
-      class=" bg-[#EDDEAA] border-2 border-black  flex flex-col justify-center items-center rounded-lg w-[26vh] h-[32vh] ">
+      class=" bg-gradient-to-b from-[#6C5F5B] to-[#DAC0A3] border-2 border-black   flex flex-col justify-center items-center rounded-lg w-[26vh] h-[32vh] ">
       <div class="w-[15vh] h-[15vh]">
         <img :src="hero.image" alt="" />
       </div>
-      <div class="flex flex-col justify-start p-5">
+      <div class="flex flex-col justify-start p-1 pt-4 text-[2vh]">
         <h2>NAME: {{ hero.name }}</h2>
         <p>HP: {{ hero.hp }}</p>
         <p>Attack: {{ hero.attack }}</p>
--- src/components/pages/Inventory.vue
+++ src/components/pages/Inventory.vue
@@ -1,13 +1,17 @@
 <script setup>
 
+import Cardcharater from "../common/Cardcharater.vue"
+
+
 </script>
 
 <template>
-    <div>
-
+    <div class=" flex flex-col justify-center  relative mx-[10vh] mt-[15vh]  h-[72vh] border-[1vh] border-[#332222] bg-gradient-to-b from-[#757A69] to-[#131411] shadow-xl shadow-black">
+        <div class="absolute px-[14vh] py-4 -top-1 left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-[#332222] bg-gradient-to-b from-[#D9D9D9] to-[#737373]">
+            <h1 class=" text-center text-5xl text-black">Inventory </h1>
+        </div>
+            <Cardcharater />
     </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
