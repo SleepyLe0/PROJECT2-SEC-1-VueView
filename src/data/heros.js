5ca9816dd765d9ecd75e63d7f5c5a0604a4d1ac0
SleepyLe0 <kunkundids@gmail.com>
2024-03-14 15:03:00 +0700

    edit heros.js

--- src/data/heros.js
+++ src/data/heros.js
@@ -4,35 +4,35 @@ const heros = [
         name: "Sword Man",
         hp: 120,
         attack: 20,
-        image: "/Hero/SwordMan.png"
+        image: "Hero/SwordMan.png"
     },
     {
         id: "2",
         name: "Sword Women",
         hp: 80,
         attack: 30,
-        image: "/Hero/SwordWomen.png"
+        image: "Hero/SwordWomen.png"
     },
     {
         id: "3",
         name: "Archer",
         hp: 60,
         attack: 50,
-        image: "/Hero/Archer.png"
+        image: "Hero/Archer.png"
     },
     {
         id: "4",
         name: "Mage",
         hp: 70,
         attack: 40,
-        image: "/Hero/Mage.png"
+        image: "Hero/Mage.png"
     },
     {
         id: "5",
         name: "Wizard",
         hp: 50,
         attack: 100,
-        image: "/Hero/Wizard.png"
+        image: "Hero/Wizard.png"
     }
 ]
 
