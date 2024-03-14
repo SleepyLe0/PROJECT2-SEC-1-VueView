208d9c0e89ac722d500f8051ff2ea6d01cd8cf7c
SleepyLe0 <kunkundids@gmail.com>
2024-03-14 14:56:47 +0700

    edit path component

--- src/components/pages/Inventory.vue
+++ src/components/pages/Inventory.vue
@@ -1,8 +1,5 @@
 <script setup>
-
-import Cardcharater from "../common/Cardcharater.vue"
-
-
+import CharacterCard from '../common/CharacterCard.vue'
 </script>
 
 <template>
@@ -10,8 +7,7 @@ import Cardcharater from "../common/Cardcharater.vue"
         <div class="absolute px-[14vh] py-4 -top-1 left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-[#332222] bg-gradient-to-b from-[#D9D9D9] to-[#737373]">
             <h1 class=" text-center text-5xl text-black">Inventory </h1>
         </div>
-        
-            <Cardcharater />
+        <CharacterCard />
     </div>
 </template>
 
--- src/components/pages/Shop.vue
+++ src/components/pages/Shop.vue
@@ -1,25 +1,25 @@
 <script setup>
-import heros from "../../assets/heros"
-import Cardcharater from "../common/Cardcharater.vue"
+import heros from '../../data/heros'
+import CharacterCard from '../common/CharacterCard.vue'
+
 const headBanner = "bg-gradient-to-b from-[#D9D9D9] to-[#737373]"
 const outerContainer = "bg-gradient-to-b from-[#757A69] to-[#131411]"
 </script>
 
 <template>
   <div class="relative mx-8 mt-28 pt-16 h-[80vh] border-4 border-black " :class="outerContainer">
-    <div
-      class="absolute px-64 py-4 -top-1 left-1/2 -translate-x-1/2 -translate-y-1/2 border-4 border-black "
+    <div class="absolute px-64 py-4 -top-1 left-1/2 -translate-x-1/2 -translate-y-1/2 border-4 border-black "
       :class="headBanner">
       <h1 class="font-main text-center text-5xl text-black">
         SHOP
       </h1>
-    </div>   
-    <div v-if="heros.pop()" > 
-      <Cardcharater />
+    </div>
+    <div v-if="heros.pop()">
+      <CharacterCard />
       <button class="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
         Buy Now
       </button>
-      </div>
+    </div>
   </div>
 </template>
 
