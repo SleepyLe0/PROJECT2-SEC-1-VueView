ed4f8bb7964803830bed3e01d05b3fd23f927cb1
SleepyLe0 <kunkundids@gmail.com>
2024-03-14 14:43:05 +0700

    level selected

--- src/components/pages/Level.vue
+++ src/components/pages/Level.vue
@@ -1,10 +1,21 @@
 <script setup>
+import LevelSelected from '../level/LevelSelected.vue'
+import { userAccounts } from '../../../data/db.json'
+import { ref } from 'vue'
 
+// const props = defineProps({
+//     currentUser: {
+//         type: Object,
+//         required: true
+//     }
+// })
+
+const user = ref(userAccounts[1])
 </script>
 
 <template>
-    <div>
-
+    <div class="w-screen h-screen bg-black bg-opacity-50">
+        <LevelSelected :levels="user.levels"/>
     </div>
 </template>
 
