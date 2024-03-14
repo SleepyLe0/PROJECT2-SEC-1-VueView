0fe094bd7e3f9540d3022930e6fe3970dba8357e
Umbyp <110766551+Umbyp@users.noreply.github.com>
2024-03-14 08:46:09 +0700

    edit Cardcharacter

--- src/components/common/Cardcharater.vue
+++ src/components/common/Cardcharater.vue
@@ -5,7 +5,7 @@ import heros from '../../assets/heros'
 <template>
   <div class="flex text-black justify-center items-center gap-2 ">
     <div v-for="hero in heros" :key="hero.id"
-      class=" bg-gradient-to-b from-[#6C5F5B] to-[#DAC0A3] border-2 border-black   flex flex-col justify-center items-center rounded-lg w-[26vh] h-[32vh] ">
+      class=" bg-gradient-to-t from-[#E2C799] to-[#C08261] border-2 border-black   flex flex-col justify-center items-center rounded-lg w-[26vh] h-[32vh] shadow-xl shadow-black">
       <div class="w-[15vh] h-[15vh]">
         <img :src="hero.image" alt="" />
       </div>
