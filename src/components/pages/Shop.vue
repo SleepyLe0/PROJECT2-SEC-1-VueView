4243ae0da82eced5a8cafc35d9caefadae44d5a2
promtkup <Kantaphong.pol@gmail.com>
2024-03-19 17:21:07 +0700

    edit shop

--- src/components/pages/Shop.vue
+++ src/components/pages/Shop.vue
@@ -19,45 +19,45 @@ const cancelBuy = () => {
   isConfirmationVisible.value = false
 }
 
-const confirmBuy = async() => {
-    if (selectedHero.value) {
-        const newGold = currentUser.value.gold - 100;
-        if (newGold >= 0) {
-            currentUser.value.gold = newGold;
-            currentUser.value.characters.push(selectedHero.value);
-            await updateUser(currentUser.value)
-            localStorage.setItem('currentUser', JSON.stringify(currentUser.value));
-            isConfirmationVisible.value = false;
+const confirmBuy = async () => {
+  if (selectedHero.value) {
+    const newGold = currentUser.value.gold - 100;
+    if (newGold >= 0) {
+      currentUser.value.gold = newGold;
+      currentUser.value.characters.push(selectedHero.value);
+      await updateUser(currentUser.value)
+      localStorage.setItem('currentUser', JSON.stringify(currentUser.value));
+      isConfirmationVisible.value = false;
 
-        } else {
-            console.log("Not enough gold to buy this hero.");
-        }
     } else {
-        console.log("No hero selected.");
+      console.log("Not enough gold to buy this hero.");
     }
+  } else {
+    console.log("No hero selected.");
+  }
 }
 
 </script>
 
 <template>
-  <div class="px-[10vh]">
+  <div class="px-[6vh]">
     <BackButton path="home" />
     <div class="absolute top-[4vh] xl:top-[2vh] right-1 px-[2vh] xl:px-[4vh] bg-gradient-to-t from-[#436850] to-[#ADBC9F]  w-[14vh] h-[8vh] xl:w-[30vh] xl:h-[11vh] text-[2.5vh] xl:text-[4vh]
             flex flex-col justify-center text-black shadow-lg  border-[0.5vh] border-lime-950 rounded-[50vh] ">
-        <div class="flex ">
-            <img src="/Common/Coin.png" alt="Coin" class="w-[4vh] xl:w-[6vh]">
-            <h1>{{ currentUser.gold }}</h1>
-        </div>
-    </div>    
+      <div class="flex ">
+        <img src="/Common/Coin.png" alt="Coin" class="w-[4vh] xl:w-[6vh]">
+        <h1>{{ currentUser.gold }}</h1>
+      </div>
+    </div>
     <div
-      class="flex justify-center relative  top-[15vh] h-[72vh] border-[2vh] border-[#332222] bg-gradient-to-b from-[#757A69] to-[#131411]">
+      class="flex justify-center relative top-[18vh] h-[72vh] border-[1.5vh] lg:border-[2vh] border-[#332222] bg-gradient-to-b from-[#757A69] to-[#131411]">
       <div
-        class="absolute top-[-6vh] w-[50vh] h-[10vh] border-[#332222] border-[1vh] bg-gradient-to-b from-[#D9D9D9] to-[#737373]">
-        <h1 class="text-center text-[6vh] text-black">
+        class="absolute top-[-6vh] w-[25vh] lg:w-[50vh] h-[10vh] border-[#332222] border-[1vh] bg-gradient-to-b from-[#D9D9D9] to-[#737373]">
+        <h1 class="text-center text-[5vh] lg:text-[6vh] text-black">
           SHOP
         </h1>
       </div>
-      <div class="flex items-center gap-[3.5vh] ">
+      <div class="flex flex-wrap items-center justify-center xl:justify-start pt-[5vh] gap-[3.5vh] p-[2vh] xl:p-[7vh]  overflow-auto  h-[65vh]">
         <div v-for="character in heros">
           <div v-if="!currentUser.characters.includes(character.id)">
             <CharacterCard :heroId="character.id" />
@@ -73,18 +73,35 @@ const confirmBuy = async() => {
       </div>
     </div>
     <teleport to="body">
-      <div v-if="isConfirmationVisible" class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-        <div class="bg-gradient-to-b from-[#757A69] to-[#131411] rounded shadow-md">
-          <div class="flex justify-end mt-4">
-            <button @click="confirmBuy"
-              class="bg-[#49FF00] text-black font-bold rounded px-4 py-2">Buy</button>
-            <button @click="cancelBuy"
-              class="bg-[#FF1700] text-white font-bold rounded px-4 py-2 mr-4">Cancel</button>
+      <div v-if="isConfirmationVisible" class="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+      <div class="bg-gradient-to-b from-[#9EA392] to-[#757A69] border-[#332222] border-4 rounded-lg w-[80%] max-w-[400px] font-main">
+        <div class="flex justify-center items-center ">
+          <h2 class="text-2xl font-bold text-black p-[1.5vh] ">Confirmation</h2>
+        </div>
+        <div v-for="character in heros" :key="character.id">
+          <div v-if="character.id === selectedHero">
+            <CharacterCard :heroId="character.id" />
           </div>
         </div>
+        <div class="flex justify-center gap-3 p-[1.5vh]">
+          <button @click="confirmBuy" class="bg-[#49FF00] hover:bg-green-600 text-black font-bold rounded focus:outline-none w-[10vh] h-[5vh] ">
+            Confirm
+          </button>
+          <button @click="cancelBuy" class="bg-[#FF1700] hover:bg-red-600 text-black font-bold rounded focus:outline-none w-[10vh] h-[5vh]">
+            Cancel
+          </button>
+        </div>
       </div>
+    </div>
+  
     </teleport>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=MedievalSharp&display=swap');
+
+.font-main {
+  font-family: "MedievalSharp", cursive;
+}
+</style>
