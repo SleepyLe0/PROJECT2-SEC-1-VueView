474d24cced286050ee9c104b74f40e3d9e58e076
promtkup <Kantaphong.pol@gmail.com>
2024-03-19 16:16:08 +0700

    edit shop delete coin

--- src/components/pages/Shop.vue
+++ src/components/pages/Shop.vue
@@ -2,15 +2,14 @@
 import { ref } from 'vue'
 import CharacterCard from '../common/CharacterCard.vue'
 import BackButton from '../common/BackButton.vue'
-import coin from '../shop/Coin.vue'
 import heros from '../../data/heros'
+import { updateUser } from '../../libs/FetchAPI'
 
 const currentUser = ref(JSON.parse(localStorage.getItem('currentUser')))
 
 const isConfirmationVisible = ref(false)
 
-const selectedHero = ref(null)
-
+const selectedHero = ref('')
 const conFirmBuyHero = (hero) => {
   selectedHero.value = hero
   isConfirmationVisible.value = true
@@ -20,9 +19,22 @@ const cancelBuy = () => {
   isConfirmationVisible.value = false
 }
 
-const confirmBuy = () => {
-  console.log("Buying")
-  isConfirmationVisible.value = false
+const confirmBuy = async() => {
+    if (selectedHero.value) {
+        const newGold = currentUser.value.gold - 100;
+        if (newGold >= 0) {
+            currentUser.value.gold = newGold;
+            currentUser.value.characters.push(selectedHero.value);
+            await updateUser(currentUser.value)
+            localStorage.setItem('currentUser', JSON.stringify(currentUser.value));
+            isConfirmationVisible.value = false;
+
+        } else {
+            console.log("Not enough gold to buy this hero.");
+        }
+    } else {
+        console.log("No hero selected.");
+    }
 }
 
 </script>
@@ -30,7 +42,13 @@ const confirmBuy = () => {
 <template>
   <div class="px-[10vh]">
     <BackButton path="home" />
-    <coin />
+    <div class="absolute top-[4vh] xl:top-[2vh] right-1 px-[2vh] xl:px-[4vh] bg-gradient-to-t from-[#436850] to-[#ADBC9F]  w-[14vh] h-[8vh] xl:w-[30vh] xl:h-[11vh] text-[2.5vh] xl:text-[4vh]
+            flex flex-col justify-center text-black shadow-lg  border-[0.5vh] border-lime-950 rounded-[50vh] ">
+        <div class="flex ">
+            <img src="/Common/Coin.png" alt="Coin" class="w-[4vh] xl:w-[6vh]">
+            <h1>{{ currentUser.gold }}</h1>
+        </div>
+    </div>    
     <div
       class="flex justify-center relative  top-[15vh] h-[72vh] border-[2vh] border-[#332222] bg-gradient-to-b from-[#757A69] to-[#131411]">
       <div
@@ -44,7 +62,7 @@ const confirmBuy = () => {
           <div v-if="!currentUser.characters.includes(character.id)">
             <CharacterCard :heroId="character.id" />
             <div class="flex justify-center p-4">
-              <button @click="conFirmBuyHero"
+              <button @click="conFirmBuyHero(character.id)"
                 class="flex justify-center items-center bg-[#49FF00] text-black font-bold rounded  w-[15vh] h-[5vh] gap-2">
                 <img src="/Common/Coin.png" alt="Coin" class="w-[4vh]">
                 100 $
