bf094f36630a42ae6debcf1400efe37506d22023
SleepyLe0 <kunkundids@gmail.com>
2024-03-25 00:35:52 +0700

    edit bot monster and balance character

--- src/data/heros.js
+++ src/data/heros.js
@@ -2,7 +2,7 @@ const heros = [
     {
         id: 1,
         name: "Sword Man",
-        hp: 120,
+        hp: 200,
         attack: 20,
         price: 0,
         image: "Hero/SwordMan.png"
@@ -10,33 +10,33 @@ const heros = [
     {
         id: 2,
         name: "Sword Women",
-        hp: 80,
-        attack: 30,
+        hp: 150,
+        attack: 40,
         price: 50,
         image: "Hero/SwordWomen.png"
     },
     {
         id: 3,
         name: "Archer",
-        hp: 60,
-        attack: 50,
-        price: 70,
+        hp: 50,
+        attack: 80,
+        price: 100,
         image: "Hero/Archer.png"
     },
     {
         id: 4,
         name: "Mage",
-        hp: 70,
-        attack: 40,
-        price: 100,
+        hp: 250,
+        attack: 50,
+        price: 150,
         image: "Hero/Mage.png"
     },
     {
         id: 5,
         name: "Wizard",
-        hp: 50,
-        attack: 100,
-        price: 150,
+        hp: 200,
+        attack: 70,
+        price: 200,
         image: "Hero/Wizard.png"
     }
 ]
