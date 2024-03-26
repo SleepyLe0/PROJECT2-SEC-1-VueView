55ebe05beae80953aaef2a3cd506500ec87082e2
Charoen01 <o0tongpc0o@gmail.com>
2024-03-26 17:10:55 +0700

    monsters and heros update

--- src/data/heros.js
+++ src/data/heros.js
@@ -10,32 +10,32 @@ const heros = [
     {
         id: 2,
         name: "Merchant",
-        hp: 80,
-        attack: 30,
+        hp: 150,
+        attack: 20,
         price: 50,
         image: "Hero/Merchant.png"
     },
     {
         id: 3,
         name: "Lancer",
-        hp: 60,
-        attack: 50,
+        hp: 275,
+        attack: 25,
         price: 100,
         image: "Hero/Lancer.png"
     },
     {
         id: 4,
         name: "Mage",
-        hp: 70,
-        attack: 40,
+        hp: 350,
+        attack: 30,
         price: 150,
         image: "Hero/Mage.png"
     },
     {
         id: 5,
         name: "Sword Man",
-        hp: 50,
-        attack: 100,
+        hp: 450,
+        attack: 30,
         price: 200,
         image: "Hero/SwordMan.png"
     }
