b9fc757b7f547bf813889696e8f0cd4ca093057d
SleepyLe0 <kunkundids@gmail.com>
2024-03-26 16:43:35 +0700

    edit validate login in router

--- src/router.js
+++ src/router.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { updateUser } from './libs/FetchAPI'
 import Welcome from './components/pages/Welcome.vue'
 import Home from './components/pages/Home.vue'
 import Login from './components/pages/Login.vue'
@@ -10,8 +11,20 @@ import SignUp from './components/pages/SignUp.vue'
 
 const routes = [
     { path: '/', component: Welcome },
-    { path: '/home', component: Home },
-    { path: '/login', component: Login },
+    { path: '/home', component: Home, beforeEnter: () => {
+        const currentUser = JSON.parse(localStorage.getItem('currentUser')) ?? undefined
+        if (currentUser === undefined) return '/login'
+    } },
+    { path: '/login', component: Login, beforeEnter: async (to, from) => {
+        if (from.path === '/home') {
+            const currentUser = JSON.parse(localStorage.getItem('currentUser'))
+            if (currentUser !== undefined) {
+                localStorage.removeItem('currentUser')
+                currentUser.isActive = false
+                await updateUser(currentUser)
+            }
+        }
+    } },
     { path: '/inventory', component: Inventory },
     { path: '/level', component: Level },
     { path: '/setting', component: Setting },
@@ -21,8 +34,7 @@ const routes = [
 
 const router =  createRouter({
     history: createWebHistory(),
-    routes,
-    linkActiveClass:"text-blue-600"
+    routes
 })
 
 export default router
